f3427331482b1e623a9f43a0f6aaa95030ec2416
hayyaoe <hayya947@gmail.com>
2024-05-17 09:24:29 +0700

    Initial Commit

---
+++ ManageDose/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ManageDose
+//
+//  Created by RabiRabi Channel on 17/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ManageDose/ManageDoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageDoseApp.swift
+//  ManageDose
+//
+//  Created by RabiRabi Channel on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ManageDoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
