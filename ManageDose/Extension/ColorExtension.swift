15bc9d1652eb7e657d1db2ef511f6962aca69ab2
Richie <rhermanto01@student.ciputra.ac.id>
2024-05-26 09:32:53 +0700

    Create pie chart

---
+++ ManageDose/Extension/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  ManageDose
+//
+//  Created by MacBook Pro on 24/05/24.
+//
+
+import SwiftUI
+
+struct ColorExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorExtension()
+}
