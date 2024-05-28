9f50487f1e1caf9a4e56d6acf24adf9b47511156
WillasT <wrorrong@student.ciputra.ac.id>
2024-05-28 19:27:40 +0700

    AddNewExpenseView Done

--- ManageDose/Components/AddNewExpenseCard.swift
+++ ManageDose/Components/AddNewExpenseCard.swift
@@ -8,6 +8,20 @@
 import SwiftUI
 
 struct AddNewExpenseCard: View {
+    @Binding var allFieldsFilled: Bool
+    
+    @State private var expenseCategory: String = ""
+    @State private var expenseName: String = ""
+    @State private var expenseAmount: Double = 50.000
+    @State private var expenseDate: Date = Date()
+    
+    private var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = ""
+        return formatter
+    }
+    
     var body: some View {
         VStack(
             spacing: 8
@@ -34,7 +48,7 @@ struct AddNewExpenseCard: View {
                             .font(.subheadline)
                             .fontWeight(.medium)
                             .padding(.bottom, 4)
-                        Text("Food and Drink")
+                        TextField("Food and Drink", text: $expenseCategory)
                             .font(.caption)
                             .foregroundStyle(.gray)
                         SeparatorBar()
@@ -47,7 +61,7 @@ struct AddNewExpenseCard: View {
                     .font(.subheadline)
                     .fontWeight(.medium)
                     .padding(.bottom, 4)
-                Text("Ex: Denver Food")
+                TextField("Ex: Denver Food", text: $expenseName)
                     .font(.caption)
                     .foregroundStyle(.gray)
                 SeparatorBar()
@@ -57,44 +71,44 @@ struct AddNewExpenseCard: View {
                     .font(.subheadline)
                     .fontWeight(.medium)
                     .padding(.bottom, 4)
-                Text("Rp 0")
+                TextField("Rp 0", value: $expenseAmount, formatter: numberFormatter)
                     .font(.caption)
+                    .foregroundStyle(.gray)
                 SeparatorBar()
                     .padding(.bottom, 8)
                 
-                Text("Expense Date")
-                    .font(.subheadline)
-                    .fontWeight(.medium)
-                    .padding(.bottom, 4)
-                Text("24 May 2024")
-                    .font(.caption)
-                SeparatorBar()
-                    .padding(.bottom, 8)
+                HStack {
+                    Text("Expense Date")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                        .padding(.bottom, 4)
+                    DatePicker("", selection: $expenseDate, displayedComponents: .date)
+                        .font(.caption)
+                    Spacer()
+                }
                 
-                //                Text("Basic Needs")
-                //                    .font(.subheadline)
-                //                    .foregroundStyle(.gray)
-                //                Text("Rp. 3.343.000")
-                //                    .fontWeight(.bold)
-                //                    .font(.subheadline)
-                //                    .truncationMode(.middle)
             }
-            
+            .onChange(of: expenseName) { checkAllFields() }
+            .onChange(of: expenseAmount) { checkAllFields() }
+            .onChange(of: expenseCategory) { checkAllFields() }
             
         }
         .padding(15)
         .background(Color.white)
-        .clipShape(
-            .rect(
-                topLeadingRadius: 36,
-                topTrailingRadius: 36
-            )
-        )
-        .shadow(color: Color.black.opacity(0.15), radius: 4, x: 0, y: 0)
+        //        .clipShape(
+        //            .rect(
+        //                topLeadingRadius: 36,
+        //                topTrailingRadius: 36
+        //            )
+        //        )
+        //        .shadow(color: Color.black.opacity(0.15), radius: 4, x: 0, y: 0)
         
+    }
+    private func checkAllFields() {
+        allFieldsFilled = !expenseName.isEmpty && expenseAmount != 0 && !expenseCategory.isEmpty
     }
 }
 
 #Preview {
-    AddNewExpenseCard()
+    AddNewExpenseCard(allFieldsFilled: .constant(false))
 }
--- ManageDose/Views/AddNewExpenseView.swift
+++ ManageDose/Views/AddNewExpenseView.swift
@@ -8,21 +8,69 @@
 import SwiftUI
 
 struct AddNewExpenseView: View {
+    @State private var showSheet = false
+    @State private var allFieldsFilled = false
+
     var body: some View {
-        Button(action: {
-            // Your button action here
-        }) {
-            Text("Add New Transaction")
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(48)
+        
+        VStack{
+            BudgetProgressCard(colorAlert: "#1FCA9D")
+                .padding(.horizontal)
+            Button(action: {
 
+            }) {
+                Text("Add New Transaction")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color(red: 83/255, green: 57/255, blue: 238/255, opacity: 1))
+                    .cornerRadius(24)
+                
+            }
+            .padding()
+            Rectangle()
+                .fill(Color(red: 220/255, green: 213/255, blue: 255/255, opacity: 1))
+                .frame(width: 500, height: 20)
+                .padding(10)
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    showSheet.toggle()
+                }) {
+                    ZStack {
+                        Circle()
+                            .fill(Color(red: 83/255, green: 57/255, blue: 238/255, opacity: 1))
+                            .frame(width: 60, height: 60)
+                        Image(systemName: "plus")
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+            .padding(36)
+            Button(action: {
+
+            }) {
+                Text("Add Expense")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(allFieldsFilled ? Color(red: 83/255, green: 57/255, blue: 238/255, opacity: 1) : Color.gray)
+                    .cornerRadius(24)
+                
+            }
+            .padding(12)
+            .disabled(!allFieldsFilled)
+            
         }
-        .padding()
-
-        Spacer()
-        AddNewExpenseCard()
+        .sheet(isPresented: $showSheet, content: {
+            AddNewExpenseCard(allFieldsFilled: $allFieldsFilled)
+                .presentationDetents([.height(360)])
+            
+        })
+        
+        
     }
 }
 
