cc1c70161cf375a10f88ec40556462ab41b24a9a
hayyaoe <hayya947@gmail.com>
2024-05-30 14:13:48 +0700

    feature: added category enum for transaction

--- ManageDose/Models/BudgetingData.swift
+++ ManageDose/Models/BudgetingData.swift
@@ -13,19 +13,34 @@ enum Budget: String, Codable{
     case saving = "Savings"
     case dailyneeds = "Daily Needs"
     case wants = "Wants"
+    
+}
+
+
+enum Category: String, Codable {
+    
+    case food = "Food"
+    case electricity = "Electric Bill"
+    
 }
 
 @Model
 class BudgetingData: Identifiable{
+    
     @Attribute(.unique) var id: String
     var name: String
     var percentage: Double
     var budget: Budget
+    var category: Category
     
-    init(id: String, name: String, percentage: Double, budget: Budget) {
+    init(id: String, name: String, percentage: Double, budget: Budget, category: Category) {
+        
         self.id = id
         self.name = name
         self.percentage = percentage
         self.budget = budget
+        self.category = category
+        
     }
+    
 }
