bf1214d8603f83fbab9aeaddb05dec0b4e39100a
WillasT <wrorrong@student.ciputra.ac.id>
2024-06-05 20:14:26 +0700

    Fix Mac

---
+++ ManageDoseMac/ManageDoseMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageDoseMacApp.swift
+//  ManageDoseMac
+//
+//  Created by MacBook Pro on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ManageDoseMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
