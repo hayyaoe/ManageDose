00b06213bec7199dc50bbf03d39a42c7d82bbf4f
WillasT <wrorrong@student.ciputra.ac.id>
2024-06-06 01:22:03 +0700

    Add Expense Halfway Done

---
+++ ManageDoseMac/Views/DetailBudget.swift
@@ -0,0 +1,89 @@
+////
+////  DetailBudget.swift
+////  ManageDoseMac
+////
+////  Created by MacBook Pro on 06/06/24.
+////
+//
+//import SwiftUI
+//
+//struct DetailBudget: View {
+//    @Environment(\.modelContext) private var context
+//    @Query private var expenses: [ExpenseData]
+//    @Binding var budgeting: BudgetingData
+//    @State private var showSheet = false
+//    @State private var allFieldsFilled = false
+//    
+//    var body: some View {
+//        VStack {
+//            BudgetProgressCard(budget: budgeting.amount, used: budgeting.used, name: budgeting.name)
+//                .padding(.top, 10)
+//            Rectangle()
+//                .fill(Color(red: 220/255, green: 213/255, blue: 255/255, opacity: 1))
+//                .frame(width: 500, height: 20)
+//                .padding(10)
+//            ScrollView(.vertical, showsIndicators: false) {
+//                LazyVStack {
+//                    ForEach(expenses.filter { $0.budget.rawValue == budgeting.budget.rawValue }) { expense in
+//                        ExpenseCard(
+//                            expenseName: expense.name,
+//                            expenseCategory: expense.categoryTransaction,
+//                            expenseAmount: expense.amount,
+//                            expenseDate: expense.date,
+//                            budgetCatefory: expense.budget
+//                        )
+//                    }
+//                }
+//            }
+//            .overlay(
+//                HStack {
+//                    Spacer()
+//                    Button(action: {
+//                        showSheet.toggle()
+//                    }) {
+//                        ZStack {
+//                            Circle()
+//                                .fill(Color(red: 83/255, green: 57/255, blue: 238/255, opacity: 1))
+//                                .frame(width: 60, height: 60)
+//                            Image(systemName: "plus")
+//                                .font(.title)
+//                                .foregroundColor(.white)
+//                        }
+//                    }
+//                }
+//                .offset(x: -10, y: 150)
+//            )
+//        }
+//        .padding(.horizontal, 15)
+//        .navigationBarTitle(
+//            Text("Detail Budget")
+//                .font(.title3)
+//                .fontWeight(.semibold),
+//            displayMode: .inline
+//        )
+//        .sheet(isPresented: $showSheet) {
+//            AddNewExpenseCard(
+//                allFieldsFilled: $allFieldsFilled,
+//                budgeting: $budgeting,
+//                isIncome: false,
+//                category: budgeting.name
+//            )
+//            .presentationDetents([.height(420)])
+//        }
+//    }
+//}
+//
+//#Preview {
+//    do {
+//        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+//        let container = try ModelContainer(for: BudgetingData.self, ExpenseData.self, configurations: config)
+//        let example = BudgetingData(name: "Basic Needs", percentage: 50, budget: .dailyneeds, totalBudget: 3000000, used: 500)
+//        
+//        @State var budgetings = example
+//        
+//        return DetailBudget(budgeting: $budgetings)
+//            .modelContainer(container)
+//    } catch {
+//        fatalError("Failed to create model container")
+//    }
+//}
--- ManageDoseMac/Views/OnBoarding1View.swift
+++ ManageDoseMac/Views/OnBoarding1View.swift
@@ -8,61 +8,78 @@
 import SwiftUI
 
 struct OnBoarding1View: View {
+    
     @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
     
     var body: some View {
-        NavigationView(content: {
-            VStack{
-                ZStack{
-                    Image("Ellipse 6")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 420, height: 420)
-                        .offset(x: 100, y: -103)
-                    Image("mobile banking 1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 420, height: 420)
-                        .offset(x: 0, y: -40)
-                    
-                }
-                HStack{
-                    PageIndicatorFill()
-                    PageIndicatorBlank()
-                    PageIndicatorBlank()
-                }
-                Text("Welcome To ManageDose!")
-                    .font(.title2)
-                    .bold()
-                    .padding(.vertical, 20)
-                Text("Manage your spendings on wants, needs, and savings!")
-                    .font(.subheadline)
-                    .frame(maxWidth: 360, alignment: .center)
-                    .multilineTextAlignment(.center)
-                Button(action: {
-                    hasSeenOnboarding = true
-                }) {
-                    Text("Skip")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.gray)
-                        .background(Color.clear)
-                        .cornerRadius(20)
-                }
+        ZStack{
+            
+            
+            //        NavigationView(content: {
+            Image("Ellipse 6")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 420, height: 420)
+                .offset(x: 360, y: -270)
+//                .rotationEffect(.degrees(24))
+            Image("Ellipse 6")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 420, height: 420)
+                .offset(x: -360, y: 270)
+                .rotationEffect(.degrees(0))
+            HStack(spacing: 40) { // Added spacing between elements
+            
                 
-                NavigationLink(destination: OnBoarding2View(), label: {                Text("Next")
-                        .frame(maxWidth: 320)
-                        .padding()
+                
+                VStack{
+                    Image("mobile banking 1")
+                }
+
+                VStack(spacing: 20) { // Added spacing between text and buttons
+                    
+                    Text("Welcome To ManageDose!")
+                        .font(.title2)
+                        .bold()
+                        .padding(.vertical, 20)
+                        .foregroundColor(Color.black)
+                    Text("Manage your spendings on wants, needs, and savings!")
                         .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .background(Color(red: 83/255, green: 57/255, blue: 238/255))
-                    .cornerRadius(20)})
+                        .frame(maxWidth: 360, alignment: .center)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.black)
+                    Button(action: {
+                        hasSeenOnboarding = true
+                    }) {
+                        Text("Skip")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+                            .background(Color.clear)
+                            .cornerRadius(20)
+                    }
+                    
+                    NavigationLink(destination: OnBoarding2View(), label: {
+                        Text("Next")
+                            .frame(maxWidth: 320)
+                            .padding()
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .background(Color(red: 83/255, green: 57/255, blue: 238/255))
+                            .cornerRadius(20)
+                    })
+                }
             }
-        })
-    }    }
+            .padding(48)
+            .background(Color.white)
+            //        })
+        }.background(Color.white)
+    }
+}
+
 
 
 #Preview {
