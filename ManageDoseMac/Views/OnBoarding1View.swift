7a9d59d9224224031cc90adddf260d92fb410e1d
WillasT <wrorrong@student.ciputra.ac.id>
2024-06-05 21:26:41 +0700

    test

---
+++ ManageDoseMac/Views/OnBoarding1View.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoarding1View.swift
+//  ManageDoseMac
+//
+//  Created by MacBook Pro on 05/06/24.
+//
+
+import SwiftUI
+
+struct OnBoarding1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoarding1View()
+}
