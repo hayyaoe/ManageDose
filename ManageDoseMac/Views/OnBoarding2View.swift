ebb99d0ccbb2da6163e888ebd6a232faa6c11a09
WillasT <wrorrong@student.ciputra.ac.id>
2024-06-05 22:02:07 +0700

    Fix Mac Part 3

--- ManageDoseMac/Views/OnBoarding2View.swift
+++ ManageDoseMac/Views/OnBoarding2View.swift
@@ -1,5 +1,5 @@
 //
-//  OnBoarding1View.swift
+//  OnBoarding2View.swift
 //  ManageDoseMac
 //
 //  Created by MacBook Pro on 05/06/24.
@@ -7,37 +7,36 @@
 
 import SwiftUI
 
-struct OnBoarding1View: View {
-    @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
-    
-    var body: some View {
-        NavigationView(content: {
+struct OnBoarding2View: View {
+        @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
+        
+        var body: some View {
             VStack{
                 ZStack{
-                    Image("Ellipse 6")
+                    Image("Ellipse 7")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 420, height: 420)
-                        .offset(x: 100, y: -103)
-                    Image("mobile banking 1")
+                        .offset(x: 130, y: -200)
+                    Image("digital wallet 1")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 420, height: 420)
-                        .offset(x: 0, y: -40)
+                        .frame(width: 360, height: 360)
+                        .offset(x: 0, y: 0)
                     
                 }
                 HStack{
+                    PageIndicatorBlank()
                     PageIndicatorFill()
                     PageIndicatorBlank()
-                    PageIndicatorBlank()
                 }
-                Text("Welcome To ManageDose!")
+                Text("Manage Your Cash Flow")
                     .font(.title2)
                     .bold()
                     .padding(.vertical, 20)
-                Text("Manage your spendings on wants, needs, and savings!")
+                Text("Add, delete, and manage your income as well as expenditure!")
                     .font(.subheadline)
-                    .frame(maxWidth: 360, alignment: .center)
+                    .frame(maxWidth: 260, alignment: .center)
                     .multilineTextAlignment(.center)
                 Button(action: {
                     hasSeenOnboarding = true
@@ -51,8 +50,7 @@ struct OnBoarding1View: View {
                         .background(Color.clear)
                         .cornerRadius(20)
                 }
-                
-                NavigationLink(destination: OnBoarding2View(), label: {                Text("Next")
+                NavigationLink(destination: OnBoarding3View(), label: {                Text("Next")
                         .frame(maxWidth: 320)
                         .padding()
                         .font(.subheadline)
@@ -61,10 +59,10 @@ struct OnBoarding1View: View {
                         .background(Color(red: 83/255, green: 57/255, blue: 238/255))
                     .cornerRadius(20)})
             }
-        })
-    }    }
+        }
+    }
 
 
 #Preview {
-    OnBoarding1View()
+    OnBoarding2View()
 }
