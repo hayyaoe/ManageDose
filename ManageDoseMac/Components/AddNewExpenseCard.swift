9ae665342e967d3601c73aad43e564ff591dd162
WillasT <wrorrong@student.ciputra.ac.id>
2024-06-06 11:32:10 +0700

    Fixing Mac View

--- ManageDoseMac/Components/AddNewExpenseCard.swift
+++ ManageDoseMac/Components/AddNewExpenseCard.swift
@@ -6,11 +6,46 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddNewExpenseCard: View {
     
+//    @Environment(\.modelContext) private var context
+    @Binding var allFieldsFilled: Bool
+    @Binding var budgeting: BudgetingData
+    var isIncome: Bool
     let category : String
+    @State private var selectedOption = CategoryTransaction.electricity
     
+//    let incomeCategoryOptions: [CategoryTransaction] = [.salary, .otherIncome]
+//        let expenseCategoryOptions: [CategoryTransaction] = [.electricity, .food]
+//
+//        var options: [CategoryTransaction] {
+//            return isIncome ? incomeCategoryOptions : expenseCategoryOptions
+//        }
+    var options: [CategoryTransaction] {
+            if isIncome {
+                return incomeCategoryOptions
+            } else {
+                switch category {
+                case "Savings":
+                    return savingOptions
+                case "Basic Needs":
+                    return basicNeedsOptions
+                case "Wants":
+                    return wantsOptions
+                default:
+                    return []
+                }
+            }
+        }
+    
+        
+    let incomeCategoryOptions: [CategoryTransaction] = [.salary, .otherIncome]
+    let savingOptions: [CategoryTransaction] = [.investment, .retirement]
+    let basicNeedsOptions: [CategoryTransaction] = [.electricity, .food]
+    let wantsOptions: [CategoryTransaction] = [.entertainment, .shopping]
+        
     var icon: String {
         switch category {
         case "Basic Needs":
@@ -40,7 +75,7 @@ struct AddNewExpenseCard: View {
             
             
             VStack(alignment: .leading){
-                Text("Add Expense")
+                Text(isIncome ? "Add Income" : "Add Expense")
                     .foregroundColor(Color.black)
                     .font(.title2)
                     .fontWeight(.bold)
@@ -52,7 +87,7 @@ struct AddNewExpenseCard: View {
                         .foregroundStyle(.blue)
                         .padding(EdgeInsets(top:0, leading:0, bottom: 10, trailing: 0))
                     VStack(alignment: .leading){
-                        Text("Expense Category")
+                        Text("\(isIncome ? "Income" : "Expense") Name")
                             .foregroundColor(Color.black)
                             .font(.subheadline)
                             .fontWeight(.medium)
@@ -60,10 +95,51 @@ struct AddNewExpenseCard: View {
                         TextField("Food and Drink", text: $expenseName)
                             .font(.subheadline)
                             .foregroundColor(Color.black)
-                            .foregroundStyle(.gray)
                             SeparatorBar()
                             .padding(.bottom, 8)
                     }
+                }
+                Text("\(isIncome ? "Income" : "Expense") Category")
+                    .foregroundColor(Color.black)
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .padding(.bottom, 4)
+                
+                Picker("Please choose an option", selection: $selectedOption) {
+                    ForEach(options, id: \.self) { option in
+                        Text(option.rawValue)
+                    }
+                }
+                .pickerStyle(MenuPickerStyle())
+                .frame(width: 360, height: 30)
+                .background(Color.white.opacity(0.2))
+                .cornerRadius(5)
+                .shadow(color: .white, radius: 2, x: 0, y: 0.1)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.blue, lineWidth: 1)
+                    )
+                    .padding(.bottom, 5)
+                
+                Text("\(isIncome ? "Income" : "Expense") Amount")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .padding(.bottom, 4)
+                TextField("Rp 0", value: $expenseAmount, formatter: numberFormatter)
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+//                    .keyboardType(.numberPad)
+                SeparatorBar()
+                    .padding(.bottom, 8)
+                
+                HStack {
+                    Text("\(isIncome ? "Income" : "Expense") Date")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                        .padding(.bottom, 4)
+                    DatePicker("", selection: $expenseDate, displayedComponents: .date)
+                        .font(.caption)
+                    Spacer()
                 }
             }
             .padding()
@@ -73,5 +149,16 @@ struct AddNewExpenseCard: View {
 }
 
 #Preview {
-    AddNewExpenseCard(category: "Savings")
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: BudgetingData.self, configurations: config)
+        let example = BudgetingData(name: "Basic Needs", percentage: 50, budget: .dailyneeds, totalBudget: 3000000, used: 500)
+        
+        @State var budgetings = example
+        
+        return AddNewExpenseCard(allFieldsFilled: .constant(false), budgeting: $budgetings, isIncome: true, category: "Basic Needs")
+            .modelContainer(container)
+    } catch {
+        fatalError("Failed to create model container")
+    }
 }
--- ManageDoseMac/Views/OnBoarding1View.swift
+++ ManageDoseMac/Views/OnBoarding1View.swift
@@ -12,70 +12,69 @@ struct OnBoarding1View: View {
     @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
     
     var body: some View {
-        ZStack{
-            
-            
-            //        NavigationView(content: {
-            Image("Ellipse 6")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 420, height: 420)
-                .offset(x: 360, y: -270)
-//                .rotationEffect(.degrees(24))
-            Image("Ellipse 6")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 420, height: 420)
-                .offset(x: -360, y: 270)
-                .rotationEffect(.degrees(0))
-            HStack(spacing: 40) { // Added spacing between elements
-            
-                
-                
-                VStack{
-                    Image("mobile banking 1")
-                }
-
-                VStack(spacing: 20) { // Added spacing between text and buttons
+        VStack{
+            ZStack{
+                //        NavigationView(content: {
+                Image("Ellipse 6")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 420, height: 420)
+                    .offset(x: 360, y: -270)
+                //                .rotationEffect(.degrees(24))
+                Image("Ellipse 6")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 420, height: 420)
+                    .offset(x: -360, y: 270)
+                    .rotationEffect(.degrees(0))
+                HStack(spacing: 40) { // Added spacing between elements
                     
-                    Text("Welcome To ManageDose!")
-                        .font(.title2)
-                        .bold()
-                        .padding(.vertical, 20)
-                        .foregroundColor(Color.black)
-                    Text("Manage your spendings on wants, needs, and savings!")
-                        .font(.subheadline)
-                        .frame(maxWidth: 360, alignment: .center)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color.black)
-                    Button(action: {
-                        hasSeenOnboarding = true
-                    }) {
-                        Text("Skip")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.gray)
-                            .background(Color.clear)
-                            .cornerRadius(20)
+                    
+                    VStack{
+                        Image("mobile banking 1")
                     }
                     
-                    NavigationLink(destination: OnBoarding2View(), label: {
-                        Text("Next")
-                            .frame(maxWidth: 320)
-                            .padding()
+                    VStack(spacing: 20) { // Added spacing between text and buttons
+                        
+                        Text("Welcome To ManageDose!")
+                            .font(.title2)
+                            .bold()
+                            .padding(.vertical, 20)
+                            .foregroundColor(Color.black)
+                        Text("Manage your spendings on wants, needs, and savings!")
                             .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                            .background(Color(red: 83/255, green: 57/255, blue: 238/255))
-                            .cornerRadius(20)
-                    })
+                            .frame(maxWidth: 360, alignment: .center)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color.black)
+                        Button(action: {
+                            hasSeenOnboarding = true
+                        }) {
+                            Text("Skip")
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.gray)
+                                .background(Color.clear)
+                                .cornerRadius(20)
+                        }
+                        
+                        NavigationLink(destination: OnBoarding2View(), label: {
+                            Text("Next")
+                                .frame(maxWidth: 320)
+                                .padding()
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                                .background(Color(red: 83/255, green: 57/255, blue: 238/255))
+                                .cornerRadius(20)
+                        })
+                    }
                 }
+                .padding(48)
+                .background(Color.white)
+                //        })
             }
-            .padding(48)
-            .background(Color.white)
-            //        })
         }.background(Color.white)
     }
 }
